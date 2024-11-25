efc2c615ab6dfb4ea76a48a03bf1a85165c3f4d6
julenbhy <julenbhy@gmail.com>
2024-11-25 13:05:56 +0100

    fix unused runtime warning

--- ow-executor/src/bin/executor.rs
+++ ow-executor/src/bin/executor.rs
@@ -1,13 +1,12 @@
 use ow_executor::core;
-// use tide_tracing::TraceMiddleware;
-// use tracing::Level;
 
 static ADDRESS: &str = "127.0.0.1:9000";
 
 #[async_std::main]
 async fn main() -> anyhow::Result<()> {
 
-    #[cfg(feature = "wasmtime")]
+    
+    #[cfg(all(feature = "wasmtime", not(feature = "wasmtime_component"), not(feature = "wasmtime_nn")))]
     let runtime = ow_wasmtime::Wasmtime::default();
 
     #[cfg(feature = "wasmtime_component")]
@@ -16,17 +15,8 @@ async fn main() -> anyhow::Result<()> {
     #[cfg(feature = "wasmtime_nn")]
     let runtime = ow_wasmtime_nn::Wasmtime::default();
 
-
-
-
-    // let subscriber = tracing_subscriber::fmt()
-    //     .with_max_level(Level::TRACE)
-    //     .finish();
-
-    // tracing::subscriber::set_global_default(subscriber).expect("no global subscriber has been set");
-
+    
     let mut executor = tide::with_state(runtime);
-    // executor.with(TraceMiddleware::new());
 
     executor.at("/:container_id/destroy").post(core::destroy);
     executor.at("/:container_id/init").post(core::init);
